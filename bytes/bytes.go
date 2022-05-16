8c0ddf32e6c75eb2b8d4f8080a03bf02cd9a8632
ShogoTakeuchi <smth.1.23@icloud.com>
2022-05-16 14:40:20 +0900

    small fix

--- bytes/bytes.go
+++ bytes/bytes.go
@@ -16,9 +16,8 @@ func main() {
 
 	println(len(abyte), "バイト")
 	println(utf8.RuneCountInString(abyte), "文字")
-	str := `development/downloads/purchased_ticket/mt_tv_tickets_11501_
-		えーっとこの度おぱなちゃん生誕祭おかげさまで2回目を迎えることになりました(パチパチパチパチ) 皆様に愛され甘やかされ(えへへ)成長していく姿をとくとご覧あれ2022
-		_aj4O0fmaDwlWmWu.xlsx`
+	str := `アリスは川辺でおねえさんのよこにすわって、なんにもすることがないのでとても退屈（たいくつ）しはじめていました。
+		一、二回はおねえさんの読んでいる本をのぞいてみたけれど、そこには絵も会話もないのです。`
 	buf := []byte(str)
 
 	str1 := url.QueryEscape(str)
